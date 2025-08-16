a678f9087a7f0bd6a1154978558d7ccfb50bedf3
furuhashi <falloutkid.ub@gmain.com>
2025-08-16 16:31:05 +0900

    feat: 🎸 implement convert string

--- sources/semantic_version/src/lib.rs
+++ sources/semantic_version/src/lib.rs
@@ -1,3 +1,8 @@
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+pub struct ParseVersionError;
+
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub struct Version {
     pub major: u64,
@@ -5,12 +10,76 @@ pub struct Version {
     pub patch: u64,
 }
 
+impl Version {
+    pub fn new(major: u64, minor: u64, patch: u64) -> Self {
+        Self {
+            major,
+            minor,
+            patch,
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        format!("{}.{}.{}", self.major, self.minor, self.patch)
+    }
+}
+
+impl FromStr for Version {
+    // エラー型として上で定義したものを指定
+    type Err = ParseVersionError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // 1. 文字列を"."で分割する
+        let parts: Vec<&str> = s.split('.').collect();
+
+        // 2. 要素が3つでなければエラー
+        if parts.len() != 3 {
+            return Err(ParseVersionError);
+        }
+
+        // 3. 各要素をu64にパースする
+        //    どれか一つでもパースに失敗したらエラー
+        let major = parts[0].parse::<u64>().map_err(|_| ParseVersionError)?;
+        let minor = parts[1].parse::<u64>().map_err(|_| ParseVersionError)?;
+        let patch = parts[2].parse::<u64>().map_err(|_| ParseVersionError)?;
+
+        // 4. パースした値でVersion構造体を作成して返す
+        Ok(Version {
+            major,
+            minor,
+            patch,
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        // unimplemented!();
+    fn to_string_test() {
+        let version = Version::new(1, 2, 3);
+        assert_eq!(version.to_string(), "1.2.3");
+    }
+
+    #[test]
+    fn test_parse_valid_version() {
+        //      .parse()メソッドを呼び出すと、内部でFromStr::from_str      が呼ばれる
+        let version: Version = "1.2.3".parse().unwrap();
+        assert_eq!(
+            version,
+            Version {
+                major: 1,
+                minor: 2,
+                patch: 3
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_invalid_version() {
+        //      不正な文字列をパースするとErrが返ることを確認
+        let result: Result<Version, _> = "1.2.a".parse();
+        assert_eq!(result, Err(ParseVersionError));
     }
 }
